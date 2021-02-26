b7e79d7709ec943021488caf26069674705d4876
rhitwikpatowary <rhitwikpatowary@gmail.com>
2021-02-25 22:37:45 -0500

    Checking in Basefiles

---
+++ WebAppTest/Controllers/BootStrapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppTest.Controllers
+{
+    public class BootStrapController : Controller
+    {
+        // GET: BootStrap
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
